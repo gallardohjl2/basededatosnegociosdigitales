a93ef2382a889068c4595b233a75e60a40d5f803
Jose Luis Gallardo <gallardohjl2@gmail.com>
2025-02-14 16:54:35 -0600

    Logro: consultas intermedias con inner join terminadas y comienzo de avanzadas

--- UnidadII/03-consultas-avanzadas.sql
+++ UnidadII/03-consultas-avanzadas.sql
@@ -201,11 +201,6 @@ on o.EmployeeID = e.EmployeeID
  on od.OrderID = o.OrderID
  group by e.FirstName, e.LastName
  
-
-
-
-
-
  select count(*)
  from Orders as o
  where EmployeeID = 1
@@ -232,9 +227,139 @@ on o.EmployeeID = e.EmployeeID
  inner join Employees as e
  on e.EmployeeID = o.EmployeeID
 
+ -- 11. Listar los clientes y la 
+ -- cantidad de pedidos que han realizado
+  select c.CompanyName as [Cliente], 
+ count(o.OrderID) as [Numero de Ordenes] 
+ from Customers as c
+ inner join Orders as o
+ on c.CustomerID = o.CustomerID
+ group by c.CompanyName
+ order by [Numero de Ordenes] desc
+
+ select c.CompanyName as [Cliente], 
+ count(*) as [Numero de Ordenes] 
+ from Customers as c
+ inner join Orders as o
+ on c.CustomerID = o.CustomerID
+ group by c.CompanyName
+ order by [Numero de Ordenes] desc
+
+ select c.CompanyName as [Cliente], 
+ count(*) as [Numero de Ordenes] 
+ from Customers as c
+ inner join Orders as o
+ on c.CustomerID = o.CustomerID
+ group by c.CompanyName
+ order by 2 desc
+
+ select c.CompanyName as [Cliente], 
+ count(*) as [Numero de Ordenes] 
+ from Customers as c
+ inner join Orders as o
+ on c.CustomerID = o.CustomerID
+ group by c.CompanyName
+ order by count(*) desc
+
+ -- 12. Obtener los empleados que han gestionado pedidos
+ -- enviados a alemania
+
+ SELECT concat(e.FirstName, ' ', e.LastName) as [Nombre]
+ FROM Employees as e
+ INNER JOIN orders as o
+ ON e.EmployeeID = o.EmployeeID
+ where o.ShipCountry = 'Germany'
+
+
+ SELECT distinct concat(e.FirstName, ' ', e.LastName) as [Nombre]
+ FROM Employees as e
+ INNER JOIN orders as o
+ ON e.EmployeeID = o.EmployeeID
+ where o.ShipCountry = 'Germany'
+
+ -- 13. Listar los productos junto con el nombre del 
+ -- proveedor y el país de origen
+
+ select p.ProductName as [Nombre Producto], 
+ s.CompanyName as [Proveedor], 
+ s.Country as [País de Origen]
+ from Products as p
+ inner join Suppliers as s
+ on p.SupplierID = s.SupplierID
+ order by 1 asc
+
+ -- 14. Obtener los pedidos agrupados por país de envio
+
+    Select o.ShipCountry as [País de Envio], 
+	count(o.OrderID) as [Numero de Ordenes]
+	from Orders as o
+	group by o.ShipCountry 
+	order by 2 desc
+	 
+
+ -- 15. Obtener los empleados y la cantidad de territorios
+ -- en los que trabajan
+
+ select concat(e.FirstName, ' ', e.LastName) as [Nombre],
+ count(et.TerritoryID) as [Cantidad Territorios]
+ from Employees as e
+ inner join EmployeeTerritories as et
+ on e.EmployeeID = et.EmployeeID
+ group by concat(e.FirstName, ' ', e.LastName)
+
+ select concat(e.FirstName, ' ', e.LastName) as [Nombre],
+ count(et.TerritoryID) as [Cantidad Territorios]
+ from Employees as e
+ inner join EmployeeTerritories as et
+ on e.EmployeeID = et.EmployeeID
+ group by e.FirstName, e.LastName
+
+ select concat(e.FirstName, ' ', e.LastName) as [Nombre],
+ t.TerritoryDescription
+ ,count(et.TerritoryID) as [Cantidad Territorios]
+ from Employees as e
+ inner join EmployeeTerritories as et
+ on e.EmployeeID = et.EmployeeID
+ INNER JOIN Territories as t
+ on et.TerritoryID = t.TerritoryID
+ group by e.FirstName, e.LastName, t.TerritoryDescription
+ order by [Nombre], t.TerritoryDescription desc
+ -- 16. Listar las categorias y la cantidad de productos 
+ -- que contienen
+ select c.CategoryName as [Categoria], 
+ count(p.ProductID) as [Cantidad de Productos]
+ from Categories as c
+ inner join Products as p
+ on c.CategoryID = p.CategoryID
+ group by c.CategoryName
+ order by 2 desc
+
+ -- 17. Obtener la cantidad total de productos vendidos por
+ -- proveedor
+
+ select s.CompanyName as [Proveedor], 
+ sum(od.Quantity) as [Total de productos Vendidos]
+ from Suppliers as s
+ inner join Products as p
+ on s.SupplierID = p.SupplierID
+ inner join [Order Details] as od
+ on od.ProductID = p.ProductID
+ group by s.CompanyName
+ order by 2 desc
+
+
+ -- 18. Obtener la cantidad de pedidos enviados por cada 
+ -- empresa de transporte
+
+-- Consultas Avanzadas 
+
+-- 19. Obtener los clientes que han realizado pedidos con 
+-- mas de un producto
 
 
 
 
- select sum(Quantity) from [Order Details]
+
+
+
 
